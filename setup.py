60454847a7da6c55479ae0086f0594898cb3f093
sbussetti <sbussetti@freelancersunion.org>
2014-01-31 09:22:51 -0500

    setup_requires

--- setup.py
+++ setup.py
@@ -6,27 +6,26 @@ import os
 os.environ['COPY_EXTENDED_ATTRIBUTES_DISABLE'] = 'true'
 os.environ['COPYFILE_DISABLE'] = 'true'
 
+
 def setup():
-	from setuptools import setup, find_packages
-	return setup(
-		name = "django-admin-preview",
-		version = "0.1.2",
-		
-		packages = find_packages(),
-		install_requires = open('requirements.txt'),
-		entry_points = {
-			'setuptools.file_finders'	: [
-				'git = setuptools_git:gitlsfiles',
-			]
-		},
-		
-		include_package_data = True,
-		zip_safe = False,
-		
-		# metadata for upload to PyPI
-		description		= "Inline preview in the admin list view",
-		url				= "https://github.com/broderboy/django-admin-preview",
-	)
+    from setuptools import setup, find_packages
+    return setup(
+        name="django-admin-preview",
+        version="0.1.3",
+        packages=find_packages(),
+		setup_requires=["setuptools_git >= 0.4.2"],
+        install_requires=open('requirements.txt'),
+        entry_points={
+            'setuptools.file_finders': [
+                'git = setuptools_git:gitlsfiles',
+            ]
+        },
+        include_package_data=True,
+        zip_safe=False,
+        # metadata for upload to PyPI
+        description="Inline preview in the admin list view",
+        url="https://github.com/broderboy/django-admin-preview",
+    )
 
 if(__name__ == '__main__'):
-	setup()
+    setup()
